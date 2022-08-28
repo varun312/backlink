73ffe987b18dbeb72c742d2b42405515c5f7d767
deadpdf <102325980+deadpdf@users.noreply.github.com>
2022-08-28 22:17:36 +0530

    Add files via upload

--- linkid.py
+++ linkid.py
@@ -7,25 +7,27 @@ import aiohttp
 from aiohttp.client import ClientSession
 
 f = ['https://youtu.be/', 'https://mega.nz/file/', 'https://mega.nz/folder/', 'https://imgur.com/gallery/', 'https://imgur.com/a/', 'https://www.instagram.com/p/', 'https://discord.gg/', 'https://www.instagram.com/', 'https://pin.it/','https://www.youtube.com/watch?v=','https://drive.google.com/file/d']
-r_t = ["""\nif result > 50000:\n    x = 1\n""","""\nif result >= 2216:\n    x = 1\n""","""\nif result >= 2228:\n    x = 1\n""","""\nif result != 6167:\n    x = 1\n""","""\nif result != 6167:\n    x = 1\n""", """\nif result >= 220000:\n    x = 1\n""","""\nif result >= 6000:\n    x = 1\n""","""\nif result <= 300000:\n    x = 1\n""","""\nif result >= 190000:\n    x = 1\n""","""\nif result >= 600000:\n    x = 1\n""","""\nif not result <= 3100:\n    x = 1\n"""]
+r_t = ["""\nif result > 500000:\n    x = 1\n""","""\nif result >= 2216:\n    x = 1\n""","""\nif result >= 2228:\n    x = 1\n""","""\nif result != 6167:\n    x = 1\n""","""\nif result != 6167:\n    x = 1\n""", """\nif result >= 220000:\n    x = 1\n""","""\nif result >= 6000:\n    x = 1\n""","""\nif result <= 300000:\n    x = 1\n""","""\nif result >= 190000:\n    x = 1\n""","""\nif result >= 600000:\n    x = 1\n""","""\nif not result <= 3100:\n    x = 1\n"""]
 url_list = ["https://bit.ly/","https://pastebin.com/","https://imgur.com/a/","https://imgur.com/gallery/","https://imgflip.com/i/","https://youtu.be/","https://tinyurl.com/","https://goo.gl/","https://www.mediafire.com/","https://mega.nz/file/","https://mega.nz/folder/","https://gyazo.com/","https://www.dailymotion.com/video/","https://www.facebook.com/","https://www.reddit.com/","https://discord.gg/","https://www.imdb.com/title/","https://pin.it/","https://drive.google.com/file/d/","https://docs.google.com/document/d/","https://forms.gle/","https://docs.google.com/prensentation/d/","https://clyp.it/","https://www.instagram.com/","https://www.instagram.com/p/","https://isbnsearch.org/isbn/","https://far-breeze-72b.notion.site/","https://u.teknik.io/"]
-backlink = "AoDHSOd"
+backlink = input("Enter Backlink ")
 f = [x+backlink for x in f]
 url_list = [x+backlink for x in url_list]
 mainf = []
 mainstr = ''
 newf = ''
+x = 0
+result = ''
 
 async def download_link(url:str,session:ClientSession):
     async with session.get(url) as response:
-        if url in f:
-            x = 0
-            result = await response.text()
-            exec(r_t[f.index(url)])
-            print(x)
-            mainf.append(url if x == 1 else "")  
+        if url in f and response.status != 404:
+            global result, x
+            result = len(await response.text())
+            exec(r_t[f.index(url)],globals())
+            mainf.append(url if x == 1 else "")
+            # print(result, r_t[f.index(url)])
+            x = 0  
         else:  
-            print(f'{response.status} {url}')
             if response.status == 200:
                 mainf.append(url)
 async def download_all(urls:list):
@@ -43,7 +45,8 @@ def ping():
     #if len(backlink) >= 5:
     asyncio.run(download_all(url_list))
     end = time.time()
-    newf = [str(mainf.index(x))+") "+x+"\n" for x in [x for x in mainf if x!=""]]
+    newf = [str([x for x in mainf if x!=""].index(x))+") "+x+"\n" for x in [x for x in mainf if x!=""]]
+    print(mainf)
     for x in newf:
         mainstr+=str(x)
     print(mainstr)
@@ -55,10 +58,10 @@ def main(stdscr):
     while True:
         key = stdscr.getkey()
         if key.isdigit() and int(key) < len(newf):
-            subprocess.run(["powershell","-Command","start '"+str(mainf[int(key)])+"'"])
+            subprocess.run(["powershell","-Command","start '"+str([x for x in mainf if x!=""][int(key)])+"'"])
         elif key == 'a':
             for x in range(len(newf)):
-                subprocess.run(["powershell","-Command","start '"+str(mainf[x])+"'"])
+                subprocess.run(["powershell","-Command","start '"+str([x for x in mainf if x!=""][x])+"'"])
         elif key == 'q':
             sys.exit()
 
